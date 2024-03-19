6ad16eed9d705816f8b5c1223cc3e99242087bfb
Suedy Mud <suedymud@hotmail.com>
2024-03-19 16:05:09 +0100

    Projet et Chercheur : Correction pagination

--- src/components/pages/Chercheur.jsx
+++ src/components/pages/Chercheur.jsx
@@ -1,18 +1,18 @@
 import axios from "axios";
 import { useAuth0 } from "@auth0/auth0-react";
 import { useEffect, useState } from "react";
-import { ListGroup, Button } from "react-bootstrap";
-import Layout from "../structure/Layout.jsx";
+import { ListGroup } from "react-bootstrap";
 import { Link } from "react-router-dom";
+import Pagination from 'react-bootstrap/Pagination';
 
 function Chercheur() {
     const { getAccessTokenSilently } = useAuth0();
     const [data, setData] = useState([]);
-    const [currentPage, setCurrentPage] = useState(0);
+    const [currentPage, setCurrentPage] = useState('A');
     const [totalPages, setTotalPages] = useState(0);
-    const [currentLetter, setCurrentLetter] = useState(""); // Ajout de l'état pour la lettre sélectionnée
+    const [firstLetters, setFirstLetters] = useState([]);
 
-    const fetchData = async () => {
+    const fetchData = async (letter) => {
         try {
             const accessToken = await getAccessTokenSilently();
             const response = await axios.get("api/zchercheur/liste", {
@@ -20,17 +20,14 @@ function Chercheur() {
                     Authorization: `Bearer ${accessToken}`,
                 },
                 params: {
-                    page: currentPage,
+                    lettre: letter,
                 },
             });
 
             if (response.status === 200) {
                 const sortedData = response.data.content.sort((a, b) => a.nom.localeCompare(b.nom));
                 setData(sortedData);
-
-                // Recalculer le nombre total de pages après le filtrage
-                const totalFilteredPages = Math.ceil(sortedData.length / 20); // supposons que vous affichiez 20 éléments par page
-                setTotalPages(totalFilteredPages);
+                setTotalPages(response.data.totalPages);
             } else {
                 console.error("Erreur lors de la récupération des données");
             }
@@ -40,45 +37,52 @@ function Chercheur() {
     };
 
     useEffect(() => {
-        fetchData();
-    }, [currentPage, getAccessTokenSilently, currentLetter]);
+        fetchData(currentPage);
+    }, [currentPage, getAccessTokenSilently]);
 
-    // Fonction pour filtrer les données en fonction de la lettre sélectionnée
-    const filterDataByLetter = (letter) => {
-        setCurrentLetter(letter);
-        setCurrentPage(0); // Réinitialiser la pagination à la première page
+    const handlePaginationClick = (letter) => {
+        setCurrentPage(letter);
     };
+
+    const alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    const paginationItems = alphabet.split('').map((letter, index) => {
+        // Vérifier s'il y a des éléments commençant par cette lettre
+        const letterIsActive = data.some(item => item.nom.charAt(0).toUpperCase() === letter);
+        return (
+            <Pagination.Item
+                key={index}
+                active={letter === currentPage}
+                disabled={!letterIsActive}
+                onClick={() => handlePaginationClick(letter)}
+            >
+                {letter}
+            </Pagination.Item>
+        );
+    });
+
+    // Filtrer les données pour n'afficher que les éléments commençant par la lettre de la page actuelle
+    const filteredData = data.filter(item => item.nom.charAt(0).toUpperCase() === currentPage);
 
     return (
         <>
             <h2>Répertoire des Chercheurs</h2>
             <div>
-
                 <ListGroup as="ul">
-                    {data
-                        .filter((item) => currentLetter === "" || item.nom.charAt(0).toUpperCase() === currentLetter)
-                        .map((item) => (
-                            <ListGroup.Item
-                                as="li"
-                                key={item.idche}
-                                className="d-flex justify-content-between align-items-center my-1">
-                                <div>
-                                    <Link to={`/chercheur/${item.idche}`} style={{ textDecoration: "none" }}>
-                                        <p>
-                                            {item.nom} {item.prenom}
-                                        </p>
-                                    </Link>
-                                </div>
-                            </ListGroup.Item>
-                        ))}
-                </ListGroup>
-                {/* Ajout des boutons de lettre */}
-                <div className="letters-pagination">
-                    {Array.from({ length: 26 }, (_, i) => String.fromCharCode(65 + i)).map((letter) => (
-                        <Button key={letter} onClick={() => filterDataByLetter(letter)}>
-                            {letter}
-                        </Button>
+                    {filteredData.map((item) => (
+                        <ListGroup.Item
+                            as="li"
+                            key={item.idche}
+                            className="d-flex justify-content-between align-items-center my-1">
+                            <div>
+                                <Link to={`/chercheur/${item.idche}`} style={{ textDecoration: 'none' }}>
+                                    <p>{item.nom}</p>
+                                </Link>
+                            </div>
+                        </ListGroup.Item>
                     ))}
+                </ListGroup>
+                <div className="pagination">
+                    <Pagination>{paginationItems}</Pagination>
                 </div>
             </div>
         </>
--- src/components/pages/Projet.jsx
+++ src/components/pages/Projet.jsx
@@ -1,18 +1,19 @@
-import {useAuth0} from "@auth0/auth0-react";
+import { useAuth0 } from "@auth0/auth0-react";
 import axios from "axios";
-import {useEffect, useState} from "react";
-import {ListGroup} from "react-bootstrap";
+import { useEffect, useState } from "react";
+import { ListGroup } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import Pagination from "react-bootstrap/Pagination";
 
 function Projet() {
-    const {getAccessTokenSilently} = useAuth0();
+    const { getAccessTokenSilently } = useAuth0();
     const [data, setData] = useState([]);
-    const [currentPage, setCurrentPage] = useState(0);
+    const [currentPage, setCurrentPage] = useState('A');
     const [totalPages, setTotalPages] = useState(0);
+    const [firstLetters, setFirstLetters] = useState([]);
 
     useEffect(() => {
-        const fetchData = async () => {
+        const fetchData = async (letter) => {
             try {
                 const accessToken = await getAccessTokenSilently();
                 const response = await axios.get("api/zprojet/liste", {
@@ -20,14 +21,17 @@ function Projet() {
                         Authorization: `Bearer ${accessToken}`,
                     },
                     params: {
-                        page: currentPage,
-                        size: 20,
+                        lettre: letter,
                     },
                 });
                 if (response.status === 200) {
                     const sortedData = response.data.content.sort((a, b) => a.nom.localeCompare(b.nom));
                     setData(sortedData);
                     setTotalPages(response.data.totalPages);
+
+                    const letters = sortedData.map(item => item.nom.charAt(0).toUpperCase());
+                    const uniqueLetters = [...new Set(letters)];
+                    setFirstLetters(uniqueLetters);
                 } else {
                     console.error("Erreur lors de la récupération des données");
                 }
@@ -35,36 +39,44 @@ function Projet() {
                 console.error("Erreur lors de la récupération des données : ", error);
             }
         };
-        fetchData();
+        fetchData(currentPage);
     }, [currentPage, getAccessTokenSilently]);
 
-
-    const handlePaginationClick = (pageNumber) => {
-        setCurrentPage(pageNumber);
+    const handlePaginationClick = (letter) => {
+        setCurrentPage(letter);
     };
 
-    const paginationItems = [];
-    for (let number = 1; number <= totalPages; number++) {
-        paginationItems.push(
-            <Pagination.Item key={number} active={number === currentPage + 1} onClick={() => handlePaginationClick(number - 1)}>
-                {number}
-            </Pagination.Item>,
+    const alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    const paginationItems = alphabet.split('').map((letter, index) => {
+        const letterIsActive = firstLetters.includes(letter);
+        return (
+            <Pagination.Item
+                key={index}
+                active={letter === currentPage}
+                disabled={!letterIsActive}
+                onClick={() => handlePaginationClick(letter)}
+            >
+                {letter}
+            </Pagination.Item>
         );
-    }
+    });
+
+    // Filtrer les données pour n'afficher que les éléments commençant par la lettre de la page actuelle
+    const filteredData = data.filter(item => item.nom.charAt(0).toUpperCase() === currentPage);
 
     return (
         <>
             <h2>Répertoire par Projets</h2>
             <div>
                 <ListGroup as="ul">
-                    {data.map((item) => (
+                    {filteredData.map((item) => (
                         <ListGroup.Item
                             as="li"
                             key={item.idprojet}
                             className="d-flex justify-content-between align-items-center my-1"
                         >
                             <div>
-                                <Link to={`/projet/${item.idprojet}`} style={{textDecoration: 'none'}}>
+                                <Link to={`/projet/${item.idprojet}`} style={{ textDecoration: 'none' }}>
                                     <p>{item.nom}</p>
                                 </Link>
                             </div>
@@ -76,8 +88,7 @@ function Projet() {
                 </div>
             </div>
         </>
-
-);
+    );
 }
 
 export default Projet;
